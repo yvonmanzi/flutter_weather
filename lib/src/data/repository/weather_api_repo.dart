5ee3cd03a872c8cb258866681e11f1a722d485f8
Yvon <ym2637@columbia.edu>
2020-05-27 11:00:23 +0300

    data layer is done. now on to BLoc layer

---
+++ lib/src/data/repository/weather_api_repo.dart
@@ -0,0 +1,15 @@
+import 'package:flutterweather/src/data/models/weather_model.dart';
+import 'package:flutterweather/src/data/provider/weather_api_provider.dart';
+import 'package:meta/meta.dart';
+
+class WeatherRepository {
+  final WeatherApiClient weatherApiClient;
+
+  WeatherRepository({@required this.weatherApiClient})
+      : assert(weatherApiClient != null);
+
+  Future<Weather> getWeather(String city) async {
+    final int locationId = await weatherApiClient.getLocationId(city);
+    return weatherApiClient.fetchWeather(locationId);
+  }
+}
